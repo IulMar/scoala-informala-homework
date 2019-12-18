6c6dceff43519b55fd8d327f165a0dccfa105e5c
IuliaMaricescu <iulia.maricescu@gmail.com>
2019-12-18 19:44:50 +0200

    class updated

--- IntroToJava/src/com/sci/test/MainClass.java
+++ IntroToJava/src/com/sci/test/MainClass.java
@@ -2,6 +2,6 @@ package com.sci.test;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello from the other side!");
+        System.out.println("Hello from the other side! Update!");
     }
 }
